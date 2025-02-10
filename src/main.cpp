8c5cb645c2399ff16fd43963a186c06b21335af1
OriSavir <orisavir@gmail.com>
2025-02-10 15:25:45 -0500

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "../include/Portfolio.hpp"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    Portfolio portfolio("data.csv");
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+        return -1;
+    }
+    std::string filename(argv[1]);
+    Portfolio portfolio(filename);
     portfolio.print_results();
     portfolio.write_results("portfolio_results.csv");
     return 0;
